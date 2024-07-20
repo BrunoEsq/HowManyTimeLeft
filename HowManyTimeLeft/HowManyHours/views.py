07b6811978009989ca3254322ca605a4c48bd1d8
Bruno Esquiva <brunoesquiva1211@gmail.com>
2024-07-20 21:25:35 +0200

    API Terminada

--- HowManyTimeLeft/HowManyHours/views.py
+++ HowManyTimeLeft/HowManyHours/views.py
@@ -4,3 +4,5 @@ def home(request):
     return render(request, 'home.html')
 
 # API KEY 1b18eeed59504542baada61eb336bc78
+# API CALL https://api.rawg.io/api/games?key=1b18eeed59504542baada61eb336bc78
+
